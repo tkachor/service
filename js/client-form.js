ac3294096d854b4e5b15647a5bda6673d95351c7
Taras Kachor <tkachor@gmail.com>
2017-12-18 16:18:34 +0000

    Project

--- js/client-form.js
+++ js/client-form.js
@@ -1,30 +1,28 @@
-
-$(function(){
-   $('#client-form input[type=submit]').click(sendForm); 
+$(function() {
+    $('#client-form input[type=submit]').click(sendForm);
 });
 
 function sendForm(ev) {
-    const form  = document.getElementsByTagName('form')[0];
+    const form = document.getElementsByTagName('form')[0];
     if (form.checkValidity()) {
         ev.preventDefault();
         $.ajax({
-            url: "https://formspree.io/tkachor@gmail.com", 
-            method: "POST",
-            data: {
-                name: $('#name').val(),
-                phone: $('#phone').val(),
-                insrument: $('#insrument').val()
-            },
-            dataType: "json"
-        })
-         .done( () => $('#goodSend').attr('open', 'open') )
-         .fail( () => $('#badSend').attr('open', 'open') );    
+                url: "https://formspree.io/tkachor@gmail.com",
+                method: "POST",
+                data: {
+                    name: $('#name').val(),
+                    phone: $('#phone').val(),
+                    insrument: $('#insrument').val()
+                },
+                dataType: "json"
+            })
+            .done(() => $('#goodSend').attr('open', 'open'))
+            .fail(() => $('#badSend').attr('open', 'open'));
     }
+
+
 }
 
-
-
-    $('.mobile_menu_click').click(function(){
-       $('.menu__category').fadeToggle();
-    });
- 
+$('.mobileMenuClick').click(function() {
+    $('.menuCategory').fadeToggle();
+});
